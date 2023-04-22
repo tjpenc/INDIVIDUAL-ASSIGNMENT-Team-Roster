c657a5aa2cdc5c85e6b9303499aee57279e375fa
Thomas Pencarinha <tjpencarinha@gmail.com>
2023-04-22 16:20:09 -0400

    created functionality to update a team member with a pre-filling form

--- components/forms/CreateMember.js
+++ components/forms/CreateMember.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { useRouter } from 'next/router';
 import { FloatingLabel, Form, Button } from 'react-bootstrap';
 import { useAuth } from '../../utils/context/authContext';
@@ -12,10 +13,14 @@ const initialState = {
   firebaseKey: '',
 };
 
-export default function CreateMember() {
+export default function CreateMember({ member }) {
   const [formInput, setFormInput] = useState(initialState);
   const { user } = useAuth();
   const router = useRouter();
+
+  useEffect(() => {
+    if (member.firebaseKey) setFormInput(member);
+  }, [member, user]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -27,13 +32,18 @@ export default function CreateMember() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const payload = { ...formInput, uid: user.uid };
-    createMember(payload).then(({ name }) => {
-      const patchPayload = { firebaseKey: name };
-      updateMember(patchPayload).then(() => {
-        router.push('/team');
+    if (member.firebaseKey) {
+      updateMember(formInput)
+        .then(() => router.push('/team'));
+    } else {
+      const payload = { ...formInput, uid: user.uid };
+      createMember(payload).then(({ name }) => {
+        const patchPayload = { firebaseKey: name };
+        updateMember(patchPayload).then(() => {
+          router.push('/team');
+        });
       });
-    });
+    }
   };
 
   return (
@@ -71,8 +81,22 @@ export default function CreateMember() {
             required
           />
         </FloatingLabel>
-        <Button type="submit">Add Member</Button>
+        <Button type="submit">{member.firebaseKey ? 'Update' : 'Create'} Member</Button>
       </Form>
     </>
   );
 }
+
+CreateMember.propTypes = {
+  member: PropTypes.shape({
+    image: PropTypes.string,
+    name: PropTypes.string,
+    role: PropTypes.string,
+    firebaseKey: PropTypes.string,
+    uid: PropTypes.string,
+  }),
+};
+
+CreateMember.defaultProps = {
+  member: initialState,
+};
