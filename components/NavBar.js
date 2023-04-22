d66db435f31bf2e1132adb3016c9ab5772700099
Thomas Pencarinha <tjpencarinha@gmail.com>
2023-04-22 14:33:55 -0400

    created links between all the pages as well as an author card. Needed to create getAuthors api call for testing. Also got carried away and did deleteAuthor call

--- components/NavBar.js
+++ components/NavBar.js
@@ -11,14 +11,20 @@ export default function NavBar() {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Link passHref href="/">
-          <Navbar.Brand>CHANGE ME</Navbar.Brand>
+          <Navbar.Brand>TEAM ROSTER</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
             <Link passHref href="/">
-              <Nav.Link>Home</Nav.Link>
+              <Nav.Link>HOME</Nav.Link>
+            </Link>
+            <Link passHref href="/team">
+              <Nav.Link>CURRENT TEAM</Nav.Link>
+            </Link>
+            <Link passHref href="/members/new">
+              <Nav.Link>NEW MEMBER</Nav.Link>
             </Link>
             <Button variant="danger" onClick={signOut}>Sign Out</Button>
           </Nav>
---
+++ pages/members/new.js
@@ -0,0 +1,3 @@
+export default function newMember() {
+  return <h1>Create New Member</h1>;
+}
